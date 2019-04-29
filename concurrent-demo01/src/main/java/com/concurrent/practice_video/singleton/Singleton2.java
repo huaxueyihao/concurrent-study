07efb28fb670443b65f69bbd921df7820a0c3bb0
huaxueyihao <837403116@qq.com>
2019-04-29 12:43:41 +0800

    java concurrent in practice video thread synchronized lock

--- concurrent-demo01/src/main/java/com/concurrent/practice_video/singleton/Singleton2.java
+++ concurrent-demo01/src/main/java/com/concurrent/practice_video/singleton/Singleton2.java
@@ -4,12 +4,22 @@ public class Singleton2 {
 
     private Singleton2(){}
 
-    private static Singleton2 instance;
+    private static volatile Singleton2 instance;
 
+    /**
+     * 偏向锁
+     * 轻量级
+     * 自旋：消耗资源
+     * @return
+     */
     public static synchronized Singleton2 getInstance(){
         if(instance == null){
             // 线程0走进来，还未创建,线程1进来了
-            instance = new Singleton2();
+            synchronized (Singleton2.class){
+                if(instance == null){
+                    instance = new Singleton2();
+                }
+            }
         }
         return instance;
     }
