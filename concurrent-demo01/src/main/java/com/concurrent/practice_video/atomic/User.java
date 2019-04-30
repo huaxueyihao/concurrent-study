0eb084a7a88372327e9b45e202da9883beed4da0
huaxueyihao <837403116@qq.com>
2019-04-30 22:44:46 +0800

    java concurrent in practice video atomic

---
+++ concurrent-demo01/src/main/java/com/concurrent/practice_video/atomic/User.java
@@ -0,0 +1,23 @@
+package com.concurrent.practice_video.atomic;
+
+public class User {
+
+    private String name;
+    public volatile Integer age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+}
---
+++ concurrent-demo01/src/main/java/com/concurrent/practice_video/atomic/note.md
@@ -0,0 +1,13 @@
+###1、原子更新基本类型
+AtomicInteger
+AtomicLong 
+AtomicBoolean
+AtomicChar
+
+
+###2、原子更新数组
+
+###3、原子更新抽象类型
+
+###4、原子更新字段
+
